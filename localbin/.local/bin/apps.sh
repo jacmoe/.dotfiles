7cf540d186d1a8a8fc9227f61767602c5e73bfb6
jacmoe <jacmoe.dk@gmail.com>
2023-03-11 18:38:00 +0100

    XMonad : apps script -> pomodoro was not great. Backup file removed.

--- localbin/.local/bin/apps.sh
+++ localbin/.local/bin/apps.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-pomodoro\n4-thunar\n5-zigcider\n6-amphetype\n7-testemacs"
+	options="cancel\n1-firefox\n2-emacs\n3-thunar\n4-zigcider\n5-amphetype\n6-testemacs"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
 	elif [[ $selected = "2-emacs" ]]; then
 		emacsclient -c
-	elif [[ $selected = "3-pomodoro" ]]; then
-		pomotroid
-	elif [[ $selected = "4-thunar" ]]; then
+	elif [[ $selected = "3-thunar" ]]; then
 		thunar
-	elif [[ $selected = "5-zigcider" ]]; then
+	elif [[ $selected = "4-zigcider" ]]; then
 		emacs --with-profile prog
-	elif [[ $selected = "6-amphetype" ]]; then
+	elif [[ $selected = "5-amphetype" ]]; then
 		$HOME/.local/bin/amphetype
-	elif [[ $selected = "7-testemacs" ]]; then
+	elif [[ $selected = "6-testemacs" ]]; then
 		emacs --with-profile test
 	fi
 }
