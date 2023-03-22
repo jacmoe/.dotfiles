3eec16b00f7c246b41e91e70909a42b3dad9974d
jacmoe <jacmoe.dk@gmail.com>
2023-03-22 21:31:16 +0100

    Qutebrowser should be used more

--- localbin/.local/bin/apps.sh
+++ localbin/.local/bin/apps.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-thunar\n4-lagrange\n5-zigcider\n6-amphetype\n7-testemacs"
+	options="cancel\n1-firefox\n2-qutebrowser\n3-emacs\n4-thunar\n5-lagrange\n6-zigcider\n7-amphetype\n8-testemacs"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
-	elif [[ $selected = "2-emacs" ]]; then
+	elif [[ $selected = "2-qutebrowser" ]]; then
+		qutebrowser
+	elif [[ $selected = "3-emacs" ]]; then
 		emacsclient -c
-	elif [[ $selected = "3-thunar" ]]; then
+	elif [[ $selected = "4-thunar" ]]; then
 		thunar
-	elif [[ $selected = "4-lagrange" ]]; then
+	elif [[ $selected = "5-lagrange" ]]; then
 		lagrange
-	elif [[ $selected = "5-zigcider" ]]; then
+	elif [[ $selected = "6-zigcider" ]]; then
 		emacs --with-profile prog
-	elif [[ $selected = "6-amphetype" ]]; then
+	elif [[ $selected = "7-amphetype" ]]; then
 		$HOME/.local/bin/amphetype
-	elif [[ $selected = "7-testemacs" ]]; then
+	elif [[ $selected = "8-testemacs" ]]; then
 		emacs --with-profile test
 	fi
 }
