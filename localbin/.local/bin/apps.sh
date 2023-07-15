9715d6d0a2ca16a6e3298561e883e3f30adabe4b
jacmoe <jacmoe.dk@gmail.com>
2023-07-15 16:23:10 +0200

    Scripts adjusted for Suse

--- localbin/.local/bin/apps.sh
+++ localbin/.local/bin/apps.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-nautilus\n4-lagrange\n5-zigcider\n6-amphetype\n7-testemacs"
+	options="cancel\n1-firefox\n2-emacs\n3-thunar\n4-lagrange\n5-zigcider\n6-amphetype\n7-testemacs"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
 	elif [[ $selected = "2-emacs" ]]; then
 		emacsclient -c
-	elif [[ $selected = "3-nautilus" ]]; then
-		nautilus
+	elif [[ $selected = "3-thunar" ]]; then
+		thunar
 	elif [[ $selected = "4-lagrange" ]]; then
 		lagrange
 	elif [[ $selected = "5-zigcider" ]]; then
