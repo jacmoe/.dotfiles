0da62888f7f5bfc9e58c394139a59c51fa092c0d
jacmoe <jacmoe.dk@gmail.com>
2023-07-15 17:50:09 +0200

    Don't blink, Wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require 'wezterm'
 return {
   hide_tab_bar_if_only_one_tab = true,
   --Acceptable values are SteadyBlock, BlinkingBlock, SteadyUnderline, BlinkingUnderline, SteadyBar, and BlinkingBar.
-  default_cursor_style = 'BlinkingBlock',
-  cursor_blink_rate = 800,
+  --default_cursor_style = 'BlinkingBlock',
+  --cursor_blink_rate = 800,
   scrollback_lines = 3500,
   font_size = 16.0,
   --color_scheme = "Aci (Gogh)",
