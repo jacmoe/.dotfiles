e6822f36d7862b87122a2897b84386670518c7bb
jacmoe <jacmoe.dk@gmail.com>
2023-07-17 22:37:46 +0200

    Fish : Zotero added to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Suppress fish's intro message
 set fish_greeting
 
-fish_add_path -g /usr/sbin ~/.local/bin ~/bin ~/zig/zig-0.11.0-dev ~/zig/zls/zig-out/bin ~/.node/bin ~/go/bin ~/odin/Odin ~/odin/ols
+fish_add_path -g /usr/sbin ~/.local/bin ~/bin ~/Zotero_linux-x86_64 ~/zig/zig-0.11.0-dev ~/zig/zls/zig-out/bin ~/.node/bin ~/go/bin ~/odin/Odin ~/odin/ols
 set -gx PROJECT_PATHS ~/projects
 set -gx GOPATH ~/go
 
