625f4c5b21a5b615a42ce5bcf4b64c3e38a1bd59
jacmoe <jacmoe.dk@gmail.com>
2025-06-19 14:47:06 +0200

    Using SpaceFM on DWL instead of Thunar

--- bin/bin/startdwl.sh
+++ bin/bin/startdwl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-slstatus -s | dwl -s "exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP & 
-lxpolkit &"
+slstatus -s | dwl -s "exec dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XAUTHORITY XDG_CURRENT_DESKTOP GDK_BACKEND=wayland & 
+exec /usr/libexec/polkit-mate-authentication-agent-1 &"
