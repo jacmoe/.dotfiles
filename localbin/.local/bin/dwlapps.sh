178e9a6e48bda0745b478c678b67205a98a37512
jacmoe <jacmoe.dk@gmail.com>
2025-06-25 12:47:05 +0200

    DWL apps script: Spacefm is actually better than Dolphin

--- localbin/.local/bin/dwlapps.sh
+++ localbin/.local/bin/dwlapps.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-dolphin\n4-okular\n5-zigcider\n6-kdenlive\n7-ristretto\n8-kmag"
+	options="cancel\n1-firefox\n2-emacs\n3-spacefm\n4-okular\n5-zigcider\n6-kdenlive\n7-ristretto\n8-kmag"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
 	elif [[ $selected = "2-emacs" ]]; then
 		emacs-pgtk
-	elif [[ $selected = "3-dolphin" ]]; then
-		dolphin
+	elif [[ $selected = "3-spacefm" ]]; then
+		spacefm
 	elif [[ $selected = "4-okular" ]]; then
 		okular
 	elif [[ $selected = "5-zigcider" ]]; then
