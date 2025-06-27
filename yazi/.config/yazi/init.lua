9cf78ee9dd64738134c44ced1848b5ca1317f861
jacmoe <jacmoe.dk@gmail.com>
2025-06-27 16:06:19 +0200

    Yazi: bunny plugin

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,2 +1,17 @@
 require("git"):setup()
 
+require("bunny"):setup({
+  hops = {
+    { key = "~",          path = "~",              desc = "Home"         },
+    { key = "m",          path = "~/Music",        desc = "Music"        },
+    { key = "D",          path = "~/Documents",    desc = "Documents"    },
+    { key = "c",          path = "~/.config",      desc = "Config files" },
+    { key = { "p", "z" }, path = "~/projects/zixelwolf", desc = "Zixelwolf project"  },
+    -- key and path attributes are required, desc is optional
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  ephemeral = true, -- Enable ephemeral hops, default is true
+  tabs = true, -- Enable tab hops, default is true
+  notify = false, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
