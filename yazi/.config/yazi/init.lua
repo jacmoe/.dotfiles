5bd39619761726382589c103228324fc39d28bed
jacmoe <jacmoe.dk@gmail.com>
2025-06-28 23:45:02 +0200

    Yazi: show username/hostname in header, and user/group in statusbar

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -17,3 +17,24 @@ require("bunny"):setup({
   notify = false, -- Notify after hopping, default is false
   fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
 })
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
