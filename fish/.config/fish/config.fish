662cc0ee9522ca7d12c895ca79af127fcffacb84
jacmoe <jacmoe.dk@gmail.com>
2025-06-29 22:26:55 +0200

    Fish: cargo added to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Suppress fish's intro message
 set fish_greeting
 
-fish_add_path -g /usr/sbin ~/.local/bin ~/bin ~/Zotero_linux-x86_64 ~/zig/zig-0.11.0-dev ~/zig/zls/zig-out/bin ~/.node/bin ~/go/bin ~/odin/Odin ~/odin/ols ~/sources/gempress /home/moena/.config/composer/vendor/bin /home/moena/.pyenv/bin
+fish_add_path -g /usr/sbin ~/.local/bin ~/bin ~/Zotero_linux-x86_64 ~/zig/zig-0.11.0-dev ~/zig/zls/zig-out/bin ~/.node/bin ~/go/bin ~/odin/Odin ~/odin/ols ~/sources/gempress /home/moena/.config/composer/vendor/bin /home/moena/.pyenv/bin /home/moena/.cargo/bin
 
 set -gx PROJECT_PATHS ~/projects
 set -gx GOPATH ~/go
