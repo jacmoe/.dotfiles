7a6cf9766c6eccb7cc2410d72e0e7ebf2b4b951f
Jacob Moena <jacmoe.dk@gmail.com>
2025-07-09 13:17:23 +0200

    Better DWL apps rofi script

--- localbin/.local/bin/dwlapps.sh
+++ localbin/.local/bin/dwlapps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-spacefm\n4-okular\n5-zigcider\n6-kdenlive\n7-ristretto\n8-kate"
+	options="cancel\n1-firefox\n2-emacs\n3-spacefm\n4-okular\n5-kate\n6-zigcider\n7-ristretto\n8-kdenlive"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
@@ -11,14 +11,14 @@ function apps {
 		spacefm
 	elif [[ $selected = "4-okular" ]]; then
 		okular
-	elif [[ $selected = "5-zigcider" ]]; then
+	elif [[ $selected = "5-kate" ]]; then
+		kate
+	elif [[ $selected = "6-zigcider" ]]; then
 		emacs-pgtk --with-profile prog
-	elif [[ $selected = "6-kdenlive" ]]; then
-		kdenlive
 	elif [[ $selected = "7-ristretto" ]]; then
 		ristretto
-	elif [[ $selected = "8-kate" ]]; then
-		kate
+	elif [[ $selected = "8-kdenlive" ]]; then
+		kdenlive
 	fi
 }
 
