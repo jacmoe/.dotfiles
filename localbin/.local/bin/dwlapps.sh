fc9ea5a68f9e341d5654b33e58a16ab9d2669878
Jacob Moena <jacmoe.dk@gmail.com>
2025-07-17 01:30:00 +0200

    DWL apps script: Emacs switcheroo

--- localbin/.local/bin/dwlapps.sh
+++ localbin/.local/bin/dwlapps.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 function apps {
-	options="cancel\n1-firefox\n2-emacs\n3-spacefm\n4-okular\n5-odin\n6-ristretto"
+	options="cancel\n1-firefox\n2-odin\n3-spacefm\n4-okular\n5-writing\n6-ristretto"
 	selected=$(echo -e $options | /home/moena/.local/bin/dwmmenu)
 	if [[ $selected = "1-firefox" ]]; then
 		firefox
-	elif [[ $selected = "2-emacs" ]]; then
-		emacs-pgtk
+	elif [[ $selected = "2-odin" ]]; then
+		emacs-pgtk --with-profile prog
 	elif [[ $selected = "3-spacefm" ]]; then
 		spacefm
 	elif [[ $selected = "4-okular" ]]; then
 		okular
-	elif [[ $selected = "5-odin" ]]; then
-		emacs-pgtk --with-profile prog
+	elif [[ $selected = "5-writing" ]]; then
+		emacs-pgtk
 	elif [[ $selected = "6-ristretto" ]]; then
 		ristretto
 	fi
